0686a2be81fe287974130867930fcc8ce83441f4
John Wedig <johnwedig@google.com>
2021-09-14 13:56:28 -0700

    Initial D-Bus interface
    This sets up an initial D-Bus interface without any real functionality.
    It doesn't interact with any storage hardware yet.
    
    The yaml files are included temporarily until the
    phosphor-dbus-interfaces review is complete:
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-dbus-interfaces/+/48636
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: If704e69ef7225257efc7c865424df4421999f62d
    Signed-off-by: John Wedig <johnwedig@google.com>

---
+++ src/estoraged.cpp
@@ -0,0 +1,35 @@
+
+#include "estoraged.hpp"
+
+#include <iostream>
+#include <vector>
+
+namespace estoraged
+{
+
+void eStoraged::format(std::vector<uint8_t>)
+{
+    std::cerr << "Formatting encrypted eMMC" << std::endl;
+}
+
+void eStoraged::erase(std::vector<uint8_t>, EraseMethod)
+{
+    std::cerr << "Erasing encrypted eMMC" << std::endl;
+}
+
+void eStoraged::lock(std::vector<uint8_t>)
+{
+    std::cerr << "Locking encrypted eMMC" << std::endl;
+}
+
+void eStoraged::unlock(std::vector<uint8_t>)
+{
+    std::cerr << "Unlocking encrypted eMMC" << std::endl;
+}
+
+void eStoraged::changePassword(std::vector<uint8_t>, std::vector<uint8_t>)
+{
+    std::cerr << "Changing password for encrypted eMMC" << std::endl;
+}
+
+} // namespace estoraged

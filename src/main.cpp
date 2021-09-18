c094fb031c61eba2539c023c675dda3063383669
John Edward Broadbent <jebr@google.com>
2021-09-17 17:53:53 -0700

    Adds options allowing device paths to be set
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: Iacb9e71dae3c1b0ab77920fde8261befa64e6094

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,44 @@
 #include "estoraged.hpp"
 
 #include <sdbusplus/bus.hpp>
-
+#include <string>
 #include <iostream>
 
-int main()
+
+static void usage(std::string_view name)
 {
+    std::cerr
+        << "Usage: " << name
+        << "eStorageD service on the BMC\n\n"
+           "  -b <blockDevice>          The phyical encrypted device\n"
+           "                            If omitted, it will default to /dev/mmcblk0.\n"
+           "  -c <Container>            The unencryped device that will be created\n"
+           "                            If omitted, it will default to /dev/mapper/emmc0";
+}
+
+
+int main(int argc, char** argv)
+{
+
+    std::string physicalBlockDev = "/dev/mmcblk0";
+    std::string containerBlockDev = "/dev/mapper/emmc0";
+    int opt;
+    while ((opt = getopt(argc, argv, "p:h:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'b':
+                physicalBlockDev = optarg;
+                break;
+            case 'c':
+                containerBlockDev = optarg;
+                break;
+            default:
+                usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
     /* DBus path location to place the object(s) */
     constexpr auto path = "/xyz/openbmc_project/storage/dev_name";
 
@@ -19,7 +52,7 @@ int main()
     b.request_name("xyz.openbmc_project.eStoraged.dev_name");
 
     // Create an eStoraged object
-    estoraged::eStoraged es_object{b, path};
+    estoraged::eStoraged es_object{b, path, physicalBlockDev, containerBlockDev};
 
     std::cerr << "eStoraged has started" << std::endl;
 

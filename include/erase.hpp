6ad166389964ae09651bdcfd278892bd7802a9cc
John Edward Broadbent <jebr@google.com>
2021-10-14 14:03:11 -0700

    Add Erase verifyGeometry
    This confirms specified amount of the drive is accessible. The min and
    max expected drive size are set as a build configuration, and compared
    against the drive size (found by using the linux ioctl). Also adds
    testing build files, testing options, and verifyGeometry test.
    
    Tested: Ran eStoraged on a machine with an eMMC, using the following
    $ ./eStoraged -b /dev/mmcblk0 &
    $ busctl call  xyz.openbmc_project.eStoraged.mmcblk0 \
     /xyz/openbmc_project/storage/mmcblk0 \
     xyz.openbmc_project.eStoraged Erase ays 1 1 \
     xyz.openbmc_project.eStoraged.EraseMethod.VerifyGeometry
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: Ie47f8666996a6085a115d1b86f2643bc278638c5

---
+++ include/erase.hpp
@@ -0,0 +1,22 @@
+#include <string>
+
+/** @class Erase
+ *  @brief Erase object provides a base class for the specific erase types
+ */
+class Erase
+{
+  public:
+    /** @brief creates an erase object
+     *  @param inDevPath the linux path for the block device
+     */
+    Erase(std::string inDevPath) : devPath(std::move(inDevPath))
+    {}
+
+    /** @brief finds the size of the linux block device in bytes
+     *  @return size of a block device using the devPath
+     */
+    uint64_t findSizeOfBlockDevice();
+
+  protected:
+    std::string devPath; /**< The linux path for the block device*/
+};
--- src/estoraged.cpp
+++ src/estoraged.cpp
@@ -1,10 +1,15 @@
 
 #include "estoraged.hpp"
 
+#include "verifyDriveGeometry.hpp"
+
 #include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/eStoraged/error.hpp>
 
 #include <iostream>
 #include <vector>
+
+using sdbusplus::xyz::openbmc_project::eStoraged::Error::EraseError;
 
 namespace estoraged
 {
@@ -16,32 +21,44 @@ void eStoraged::format(std::vector<uint8_t>)
     lg2::info("Starting format", "REDFISH_MESSAGE_ID", msg);
 }
 
-void eStoraged::erase(std::vector<uint8_t>, EraseMethod)
+void eStoraged::erase(std::vector<uint8_t>, EraseMethod inEraseMethod)
 {
     std::cerr << "Erasing encrypted eMMC" << std::endl;
-    std::string msg = "OpenBMC.0.1.DriveErase";
-    lg2::info("Starting erase", "REDFISH_MESSAGE_ID", msg);
+    lg2::info("Starting erase", "REDFISH_MESSAGE_ID",
+              std::string("OpenBMC.0.1.DriveErase"));
+    switch (inEraseMethod)
+    {
+        case EraseMethod::VerifyGeometry:
+        {
+            VerifyDriveGeometry myVerifyGeometry(devPath);
+            uint64_t size = myVerifyGeometry.findSizeOfBlockDevice();
+            myVerifyGeometry.geometryOkay(size);
+            break;
+        }
+        default:
+            throw EraseError();
+    }
 }
 
 void eStoraged::lock(std::vector<uint8_t>)
 {
     std::cerr << "Locking encrypted eMMC" << std::endl;
-    std::string msg = "OpenBMC.0.1.DriveLock";
-    lg2::info("Starting lock", "REDFISH_MESSAGE_ID", msg);
+    lg2::info("Starting lock", "REDFISH_MESSAGE_ID",
+              std::string("OpenBMC.0.1.DriveLock"));
 }
 
 void eStoraged::unlock(std::vector<uint8_t>)
 {
     std::cerr << "Unlocking encrypted eMMC" << std::endl;
-    std::string msg = "OpenBMC.0.1.DriveUnlock";
-    lg2::info("Starting unlock", "REDFISH_MESSAGE_ID", msg);
+    lg2::info("Starting unlock", "REDFISH_MESSAGE_ID",
+              std::string("OpenBMC.0.1.DriveUnlock"));
 }
 
 void eStoraged::changePassword(std::vector<uint8_t>, std::vector<uint8_t>)
 {
     std::cerr << "Changing password for encrypted eMMC" << std::endl;
-    std::string msg = "OpenBMC.0.1.DrivePasswordChanged";
-    lg2::info("Starting change password", "REDFISH_MESSAGE_ID", msg);
+    lg2::info("Starting change password", "REDFISH_MESSAGE_ID",
+              std::string("OpenBMC.0.1.DrivePasswordChanged"));
 }
 
 } // namespace estoraged

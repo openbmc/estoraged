b6b0d24d08083d799f484d2b8162f1756b849764
John Edward Broadbent <jebr@google.com>
2021-10-19 16:16:00 -0700

    Adds test for VerifyGeomenty
    Starts the unit testing framework, and adds the meson options.
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: Id969de1b880325f8733dcd4baa9dbb6a76b05ff7

--- erase/verifyGeometry.cpp
+++ erase/verifyGeometry.cpp
@@ -7,13 +7,13 @@ int verifyGeometry::GeometryOkay(int(*findSize)(std::string, unsigned long& byte
    if (findSize(m_devPath, bytes) != 0){
        return false;
    }
-   if (bytes >= ERASE_MAX_GEOMETRY){
+   if (bytes > ERASE_MAX_GEOMETRY){
      sd_journal_send("MESSAGE=%s", "eStorageD erase verify Geometry to large",
                      "REDFISH_MESSAGE_ID=%s","eStorageD.1.0.EraseFailure",
                      "REDFISH_MESSAGE_ARGS=%d >= %d", bytes, ERASE_MAX_GEOMETRY, NULL);
       return -1;
    }
-   else if (bytes <= ERASE_MIN_GEOMETRY){
+   else if (bytes < ERASE_MIN_GEOMETRY){
       sd_journal_send("MESSAGE=%s", "eStorageD erase verify Geometry to small",
                      "REDFISH_MESSAGE_ID=%s","eStorageD.1.0.EraseFailure",
                      "REDFISH_MESSAGE_ARGS=%d <= %d", bytes, ERASE_MIN_GEOMETRY, NULL);
@@ -24,5 +24,5 @@ int verifyGeometry::GeometryOkay(int(*findSize)(std::string, unsigned long& byte
                      "REDFISH_MESSAGE_ID=%s","eStorageD.1.0.EraseSuccess",
                      "REDFISH_MESSAGE_ARGS=%d <= %d", bytes, ERASE_MIN_GEOMETRY, NULL);
    }
-      return true;
+      return 0;
 }

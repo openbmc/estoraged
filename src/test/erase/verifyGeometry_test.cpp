255853a2bea90c63eae7824248bcd38f614fbd3a
John Edward Broadbent <jebr@google.com>
2021-10-19 16:16:00 -0700

    Adds test for VerifyGeomenty
    Starts the unit testing framework, and adds the meson options.
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: Id969de1b880325f8733dcd4baa9dbb6a76b05ff7

---
+++ src/test/erase/verifyGeometry_test.cpp
@@ -0,0 +1,27 @@
+#include "estoraged_conf.hpp"
+#include "verifyDriveGeometry.hpp"
+
+#include <xyz/openbmc_project/eStoraged/error.hpp>
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+using sdbusplus::xyz::openbmc_project::eStoraged::Error::EraseError;
+
+TEST(VerifyGeometry, max)
+{
+    VerifyDriveGeometry maxVerify("");
+    EXPECT_THROW(maxVerify.geometryOkay(ERASE_MAX_GEOMETRY + 1), EraseError);
+}
+
+TEST(VerifyGeometry, min)
+{
+    VerifyDriveGeometry minVerify("");
+    EXPECT_THROW(minVerify.geometryOkay(ERASE_MIN_GEOMETRY), EraseError);
+}
+
+TEST(VerifyGeometry, pass)
+{
+    VerifyDriveGeometry passVerify("");
+    ASSERT_EQ(0, passVerify.geometryOkay(ERASE_MIN_GEOMETRY + 1));
+}

6796a8af6dfa6ba4cdfc6fc9f73c89e2572f1d91
John Edward Broadbent <jebr@google.com>
2021-11-05 13:45:45 -0700

    Add erase sanitize
    Sanitize uses the eMMC firmware to erase all blocks.
    
    Tested:
    $ cat /dev/urandom > /dev/mmcblk0
    
    $ time busctl call xyz.openbmc_project.eStoraged.mmcblk0
    /xyz/openbmc_project/storage/mmcblk0
    xyz.openbmc_project.Inventory.Item.Volume Erase s
    xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.VendorSanitize
    --timeout=1200
    
    real    0m1.793s
    user    0m0.021s
    sys     0m0.009s
    
    root@ytbaz20-nfd01:/# cat /dev/mmcblk0 | hexdump
    
    0000000 0000 0000 0000 0000 0000 0000 0000 0000
    *
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I31bc21c0b6d31cbba0db752d94a93eb004dbbde6

---
+++ include/sanitize.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "erase.hpp"
+
+#include <linux/mmc/ioctl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+#include <stdplus/fd/managed.hpp>
+
+#include <cstddef>
+#include <span>
+#include <string_view>
+
+constexpr uint32_t EXT_CSD_ERASE_GROUP_DEF = 175;
+constexpr uint32_t EXT_CSD_ERASE_TIMEOUT_MULT = 223;
+constexpr uint32_t EXT_CSD_HC_ERASE_GRP_SIZE = 224;
+
+namespace estoraged
+{
+
+using stdplus::fd::ManagedFd;
+
+class IOCTLWrapperInterface
+{
+  public:
+    /** @brief Wrapper around ioctl
+     *  @details Used for mocking purposes.
+     *
+     * @param[in] devPath - File name of block device
+     * @param[in] request - Device-dependent request code
+     * @param[in] mmc_ioc_cmd - eMMC cmd
+     */
+    virtual int doIoctl(std::string_view devPath, unsigned long request,
+                        struct mmc_ioc_cmd data) = 0;
+
+    /** @brief Wrapper around ioctl
+     *  @details Used for mocking purposes.
+     *
+     * @param[in] devPath - File name of block device
+     * @param[in] request - Device-dependent request code
+     * @param[in] mmc_io_mutli_cmd - many eMMC cmd
+     */
+    virtual int doIoctlMulti(std::string_view devPath, unsigned long request,
+                             struct mmc_io_multi_cmd_erase data) = 0;
+
+    virtual ~IOCTLWrapperInterface()
+    {}
+};
+
+// mockIOCTLWapper also inherits from IOCTLWrapperInterface
+class IOCTLWrapperImpl : public IOCTLWrapperInterface
+{
+  public:
+    int doIoctl(std::string_view devPath, unsigned long request,
+                struct mmc_ioc_cmd data) override;
+    int doIoctlMulti(std::string_view devPath, unsigned long request,
+                     struct mmc_io_multi_cmd_erase) override;
+    ~IOCTLWrapperImpl();
+};
+
+class Sanitize : public Erase
+{
+  public:
+    Sanitize(std::string_view inDevPath,
+             std::unique_ptr<IOCTLWrapperInterface> inIOCTL =
+                 std::make_unique<IOCTLWrapperImpl>()) :
+        Erase(inDevPath),
+        ioctlWrapper(std::move(inIOCTL))
+    {}
+    ~Sanitize()
+    {}
+    void doSanitize(uint64_t driveSize, std::span<std::byte>& extCsd);
+    void readExtCsd(std::span<std::byte>& extCsd);
+
+  private:
+    std::unique_ptr<IOCTLWrapperInterface> ioctlWrapper;
+    void emmcSanitize();
+    void emmcErase(uint64_t driveSize, std::span<std::byte>& extCsd);
+};
+
+// can't use the real mmc_ioc_multi_cmd b/c of zero length array
+// see uapi/linux/mmc/ioctl.h
+struct mmc_io_multi_cmd_erase
+{
+    uint64_t num_of_cmds;
+    struct mmc_ioc_cmd cmds[3];
+};
+
+} // namespace estoraged

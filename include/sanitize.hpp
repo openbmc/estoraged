079005d6c12250b5c38248e279154a5f75eb0cd3
John Edward Broadbent <jebr@google.com>
2021-11-05 13:45:45 -0700

    Add erase sanitize
    Sanitize uses the eMMC firmware to erase all blocks.
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I31bc21c0b6d31cbba0db752d94a93eb004dbbde6

---
+++ include/sanitize.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "erase.hpp"
+
+#include <linux/mmc/ioctl.h>
+#include <sys/types.h>
+
+#include <stdplus/fd/managed.hpp>
+
+#include <cstddef>
+#include <span>
+#include <string_view>
+
+using stdplus::fd::ManagedFd;
+
+class IOCTLWrapperInterface
+{
+  public:
+    virtual int doIoctl(ManagedFd& fd, unsigned long request,
+                        struct mmc_ioc_cmd data) = 0;
+    virtual int doIoctlMulti(ManagedFd& fd, unsigned long request,
+                             struct mmc_io_multi_cmd_erase data) = 0;
+    virtual ~IOCTLWrapperInterface()
+    {}
+};
+
+// mockIOCTLWapper also inherits from IOCTLWrapperInterface
+class IOCTLWrapperImpl : public IOCTLWrapperInterface
+{
+  public:
+    int doIoctl(ManagedFd& fd, unsigned long request,
+                struct mmc_ioc_cmd data) override;
+    int doIoctlMulti(ManagedFd& fd, unsigned long request,
+                     struct mmc_io_multi_cmd_erase) override;
+    ~IOCTLWrapperImpl();
+};
+
+class Sanitize : public Erase
+{
+  public:
+    Sanitize(std::string_view inDevPath,
+             std::unique_ptr<IOCTLWrapperInterface> inIOCTL =
+                 std::make_unique<IOCTLWrapperImpl>()) :
+        Erase(inDevPath),
+        ioctlWrapper(std::move(inIOCTL))
+    {}
+    ~Sanitize()
+    {}
+    void doSanitize();
+
+  private:
+    void readExtCsd(std::span<std::byte>& extCsd);
+    std::unique_ptr<IOCTLWrapperInterface> ioctlWrapper;
+};
+
+// can't use the real mmc_io_multi_cmd b/c of zero lenght array
+// see uapi/linux/mmc/ioctl.h
+struct mmc_io_multi_cmd_erase
+{
+    uint64_t num_of_cmds;
+    struct mmc_ioc_cmd cmds[3];
+};

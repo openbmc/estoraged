06eeba14e6350e8274c3858a45815694dfa890d1
John Edward Broadbent <jebr@google.com>
2021-11-05 13:45:45 -0700

    Add erase sanitize
    Sanitize uses the eMMC firmware to erase all blocks.
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I31bc21c0b6d31cbba0db752d94a93eb004dbbde6

--- src/estoraged.cpp
+++ src/estoraged.cpp
@@ -3,6 +3,7 @@
 
 #include "cryptsetupInterface.hpp"
 #include "pattern.hpp"
+#include "sanitize.hpp"
 #include "verifyDriveGeometry.hpp"
 #include "zero.hpp"
 
@@ -90,6 +91,12 @@ void eStoraged::erase(EraseMethod inEraseMethod)
         }
         case EraseMethod::VendorSanitize:
         {
+            Sanitize mySanitize(devPath);
+            uint64_t size = mySanitize.findSizeOfBlockDevice();
+            std::byte _ExtCsd[512] = {};
+            std::span<std::byte> extCsd(_ExtCsd);
+            mySanitize.readExtCsd(extCsd);
+            mySanitize.doSanitize(size, extCsd);
             break;
         }
         case EraseMethod::ZeroOverWrite:

a0098348dbf94c7495bff1aeb321539c9a0a3077
John Edward Broadbent <jebr@google.com>
2021-11-05 13:45:45 -0700

    Add erase sanitize
    Sanitize uses the eMMC firmware to erase all blocks.
    
    Tested:
    $ cat /dev/urandom > /dev/mmcblk0
    
    $ time busctl call xyz.openbmc_project.eStoraged.mmcblk0
    /xyz/openbmc_project/storage/mmcblk0
    xyz.openbmc_project.Inventory.Item.Volume Erase s
    xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.VendorSanitize
    --timeout=1200
    
    real    0m1.793s
    user    0m0.021s
    sys     0m0.009s
    
    root@ytbaz20-nfd01:/# cat /dev/mmcblk0 | hexdump
    
    0000000 0000 0000 0000 0000 0000 0000 0000 0000
    *
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I31bc21c0b6d31cbba0db752d94a93eb004dbbde6

--- src/estoraged.cpp
+++ src/estoraged.cpp
@@ -4,6 +4,7 @@
 #include "cryptErase.hpp"
 #include "cryptsetupInterface.hpp"
 #include "pattern.hpp"
+#include "sanitize.hpp"
 #include "verifyDriveGeometry.hpp"
 #include "zero.hpp"
 
@@ -88,6 +89,13 @@ void EStoraged::erase(EraseMethod inEraseMethod)
         }
         case EraseMethod::VendorSanitize:
         {
+            Sanitize mySanitize(devPath);
+            // uint64_t size = mySanitize.findSizeOfBlockDevice();
+            // std::byte _ExtCsd[512] = {};
+            // std::span<std::byte> extCsd(_ExtCsd);
+            // mySanitize.readExtCsd(extCsd);
+            mySanitize.doSanitize(mySanitize.findSizeOfBlockDevice(),
+                                  mySanitize.readExtCsd());
             break;
         }
         case EraseMethod::ZeroOverWrite:

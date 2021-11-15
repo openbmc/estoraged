a629fb64e4dc70e492e52e5743c2f65ee8626319
John Edward Broadbent <jebr@google.com>
2021-11-15 15:21:59 -0800

    Adds logging wrapper
    This commit adds a thin wrapper for logging, that will allow us to
    easily change how messages are logged, if we want to redirection how
    they are written.
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: Ideb4f6033b94dd8b1384c79cd3a26ce89e7b2c4c

--- src/estoraged.cpp
+++ src/estoraged.cpp
@@ -1,6 +1,8 @@
 
 #include "estoraged.hpp"
 
+#include "logging.hpp"
+
 #include <iostream>
 #include <vector>
 
@@ -10,26 +12,41 @@ namespace estoraged
 void eStoraged::format(std::vector<uint8_t>)
 {
     std::cerr << "Formatting encrypted eMMC" << std::endl;
+    estoragedLogging("MESSAGE=starting Format"
+                     "REDFISH_MESSAGE_ID=%s",
+                     "OpenBMC.0.1.DriveFormat", NULL);
 }
 
 void eStoraged::erase(std::vector<uint8_t>, EraseMethod)
 {
     std::cerr << "Erasing encrypted eMMC" << std::endl;
+    estoragedLogging("MESSAGE=starting Erase"
+                     "REDFISH_MESSAGE_ID=%s",
+                     "OpenBMC.0.1.DriveErase", NULL);
 }
 
 void eStoraged::lock(std::vector<uint8_t>)
 {
     std::cerr << "Locking encrypted eMMC" << std::endl;
+    estoragedLogging("MESSAGE=starting Lock"
+                     "REDFISH_MESSAGE_ID=%s",
+                     "OpenBMC.0.1.DriveLock", NULL);
 }
 
 void eStoraged::unlock(std::vector<uint8_t>)
 {
     std::cerr << "Unlocking encrypted eMMC" << std::endl;
+    estoragedLogging("MESSAGE=starting unlock"
+                     "REDFISH_MESSAGE_ID=%s",
+                     "OpenBMC.0.1.DriveUnlock", NULL);
 }
 
 void eStoraged::changePassword(std::vector<uint8_t>, std::vector<uint8_t>)
 {
     std::cerr << "Changing password for encrypted eMMC" << std::endl;
+    estoragedLogging("MESSAGE=starting chaning password"
+                     "REDFISH_MESSAGE_ID=%s",
+                     "OpenBMC.0.1.DrivePasswordChanged", NULL);
 }
 
 } // namespace estoraged

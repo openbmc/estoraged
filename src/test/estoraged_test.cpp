e38e5225d766c07d0a1bc79485c567001975884a
John Edward Broadbent <jebr@google.com>
2022-01-13 17:41:32 -0800

    Add crypto Erase to eStorageD.
    The goal is to erase the keys that are used to decrypt the drive. After
    the keys are erased it will not be possible to decrypt the drive, even
    if the password can be recalled. The data is forever inaccessible.
    
    Testing:
    
    $ systemctl stop emmc.service
    
    $ /eStoraged -b /dev/mmcblk0 &
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume FormatLuks ays 3 1 2 3 xyz.openbmc_project.Inventory.Item.Volume.FilesystemType.ext4
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Lock
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Erase s xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.CryptoErase
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Unlock ay 3 1 2 3
    Call failed: The operation failed internally.
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I3221e82a92c1b555e2379b19c9e1d5b6e4b02f9b

--- src/test/estoraged_test.cpp
+++ src/test/estoraged_test.cpp
@@ -1,4 +1,5 @@
 
+#include "cryptErase.hpp"
 #include "cryptsetupInterface.hpp"
 #include "estoraged.hpp"
 #include "filesystemInterface.hpp"
@@ -6,8 +7,11 @@
 #include <unistd.h>
 
 #include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
 #include <sdbusplus/test/sdbus_mock.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Inventory/Item/Volume/client.hpp>
+#include <xyz/openbmc_project/Inventory/Item/Volume/server.hpp>
 
 #include <exception>
 #include <filesystem>
@@ -73,6 +77,14 @@ class MockCryptsetupInterface : public estoraged::CryptsetupInterface
 
     MOCK_METHOD(int, cryptDeactivate,
                 (struct crypt_device * cd, const char* name), (override));
+
+    MOCK_METHOD(int, cryptKeyslotDestroy,
+                (struct crypt_device * cd, const int slot), (override));
+
+    MOCK_METHOD(int, cryptKeySlotMax, (const char* type), (override));
+
+    MOCK_METHOD(crypt_keyslot_info, cryptKeySlotStatus,
+                (struct crypt_device * cd, int keyslot), (override));
 };
 
 using sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
@@ -81,6 +93,7 @@ using sdbusplus::xyz::openbmc_project::Inventory::Item::server::Volume;
 using std::filesystem::path;
 using ::testing::_;
 using ::testing::ContainsRegex;
+using ::testing::Ge;
 using ::testing::IsNull;
 using ::testing::Return;
 using ::testing::StrEq;
@@ -567,6 +580,70 @@ TEST_F(eStoragedTest, DeactivateFail)
 
     EXPECT_THROW(esObject->lock(), InternalFailure);
     EXPECT_FALSE(esObject->isLocked());
+}
+
+/* Crypto erase uses the cryptIface so it requires use of the mockCryptIface */
+/* if mockCryptIface can be factored out of this file, cryptoErase can be teseed
+ * in a seperate file */
+
+TEST_F(eStoragedTest, EraseCrypPass)
+{
+    EXPECT_CALL(*mockCryptIface, cryptLoad(_, Ge(CRYPT_LUKS2), nullptr))
+        .WillOnce(Return(0));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotMax(Ge(CRYPT_LUKS2)))
+        .WillOnce(Return(1));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotStatus(_, 0))
+        .WillOnce(Return(CRYPT_SLOT_ACTIVE_LAST));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeyslotDestroy(_, 0)).Times(1);
+
+    EXPECT_NO_THROW(esObject->erase(Volume::EraseMethod::CryptoErase));
+}
+
+TEST_F(eStoragedTest, EraseCrypMaxSlotFails)
+{
+    EXPECT_CALL(*mockCryptIface, cryptLoad(_, Ge(CRYPT_LUKS2), nullptr))
+        .WillOnce(Return(0));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotMax(Ge(CRYPT_LUKS2)))
+        .WillOnce(Return(0));
+
+    EXPECT_THROW(esObject->erase(Volume::EraseMethod::CryptoErase),
+                 ResourceNotFound);
+}
+
+TEST_F(eStoragedTest, EraseCrypOnlyInvalid)
+{
+    EXPECT_CALL(*mockCryptIface, cryptLoad(_, Ge(CRYPT_LUKS2), nullptr))
+        .WillOnce(Return(0));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotMax(Ge(CRYPT_LUKS2)))
+        .WillOnce(Return(1));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotStatus(_, 0))
+        .WillOnce(Return(CRYPT_SLOT_INVALID));
+
+    EXPECT_NO_THROW(esObject->erase(Volume::EraseMethod::CryptoErase));
+}
+
+TEST_F(eStoragedTest, EraseCrypDestoryFails)
+{
+    EXPECT_CALL(*mockCryptIface, cryptLoad(_, Ge(CRYPT_LUKS2), nullptr))
+        .WillOnce(Return(0));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotMax(Ge(CRYPT_LUKS2)))
+        .WillOnce(Return(1));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeySlotStatus(_, 0))
+        .WillOnce(Return(CRYPT_SLOT_ACTIVE));
+
+    EXPECT_CALL(*mockCryptIface, cryptKeyslotDestroy(_, 0))
+        .WillOnce(Return(-1));
+
+    EXPECT_THROW(esObject->erase(Volume::EraseMethod::CryptoErase),
+                 InternalFailure);
 }
 
 } // namespace estoraged_test

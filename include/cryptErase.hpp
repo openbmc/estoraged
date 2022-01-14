2b8bfada73d526c1708426794ec51a35fc8ab7d5
John Edward Broadbent <jebr@google.com>
2022-01-13 17:41:32 -0800

    Add crypto Erase to eStorageD.
    The goal is to erase the keys that are used to decrypt the drive. After
    the keys are erased it will not be possible to decrypt the drive, even
    if the password can be recalled. The data is forever inaccessible.
    
    Testing:
    
    $ systemctl stop emmc.service
    
    $ /eStoraged -b /dev/mmcblk0 &
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume FormatLuks ays 3 1 2 3 xyz.openbmc_project.Inventory.Item.Volume.FilesystemType.ext4
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Lock
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Erase s xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.CryptoErase
    
    $ busctl call xyz.openbmc_project.eStoraged.mmcblk0 /xyz/openbmc_project/storage/mmcblk0 xyz.openbmc_project.Inventory.Item.Volume Unlock ay 3 1 2 3
    Call failed: The operation failed internally.
    
    Signed-off-by: John Edward Broadbent <jebr@google.com>
    Change-Id: I3221e82a92c1b555e2379b19c9e1d5b6e4b02f9b

---
+++ include/cryptErase.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cryptsetupInterface.hpp"
+#include "erase.hpp"
+
+#include <libcryptsetup.h>
+
+#include <memory>
+#include <string_view>
+namespace estoraged
+{
+
+class CryptErase : public Erase
+{
+  public:
+    CryptErase(std::string_view devPath,
+               std::unique_ptr<estoraged::CryptsetupInterface> cryptIface =
+                   std::make_unique<Cryptsetup>());
+};
+
+} // namespace estoraged

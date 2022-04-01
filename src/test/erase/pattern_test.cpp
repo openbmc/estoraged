7eade3bcdd63650e21f31a7e577f2200a0907dc3
John Edward Broadbent <jebr@google.com>
2022-03-31 19:33:21 -0700

    Fix Short read/write issue
    It was possible for "short" reads and writes to cause the pattern and
    zero steps to not work correctly. This change adds logic to deal with
    the short reads.
    
    Change-Id: If8df9bdba159a3bcfa77104a4c17b8d352794db2
    Signed-off-by: John Edward Broadbent <jebr@google.com>

--- src/test/erase/pattern_test.cpp
+++ src/test/erase/pattern_test.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #include <stdplus/fd/create.hpp>
+#include <stdplus/fd/gmock.hpp>
 #include <stdplus/fd/managed.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 
@@ -20,6 +21,8 @@ namespace estoraged_test
 
 using estoraged::Pattern;
 using sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+using testing::_;
+using testing::Return;
 
 TEST(pattern, patternPass)
 {
@@ -30,9 +33,19 @@ TEST(pattern, patternPass)
                   std::ios::out | std::ios::binary | std::ios::trunc);
     testFile.close();
 
+    stdplus::fd::Fd&& writeFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::WriteOnly);
+
     Pattern pass(testFileName);
-    EXPECT_NO_THROW(pass.writePattern(size));
-    EXPECT_NO_THROW(pass.verifyPattern(size));
+    std::cerr << "about to write" << std::endl;
+    EXPECT_NO_THROW(pass.writePattern(size, writeFd));
+
+    stdplus::fd::Fd&& readFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::ReadOnly);
+
+    std::cerr << "about to verify" << std::endl;
+    EXPECT_NO_THROW(pass.verifyPattern(size, readFd));
+    std::cerr << "done verify" << std::endl;
 }
 
 /* This test that pattern writes the correct number of bytes even if
@@ -48,9 +61,14 @@ TEST(pattern, patternNotDivisible)
                   std::ios::out | std::ios::binary | std::ios::trunc);
     testFile.close();
 
+    stdplus::fd::Fd&& writeFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::WriteOnly);
     Pattern pass(testFileName);
-    EXPECT_NO_THROW(pass.writePattern(size));
-    EXPECT_NO_THROW(pass.verifyPattern(size));
+    EXPECT_NO_THROW(pass.writePattern(size, writeFd));
+
+    stdplus::fd::Fd&& readFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::ReadOnly);
+    EXPECT_NO_THROW(pass.verifyPattern(size, readFd));
 }
 
 TEST(pattern, patternsDontMatch)
@@ -67,8 +85,118 @@ TEST(pattern, patternsDontMatch)
                    sizeof(dummyValue));
     testFile.close();
 
-    EXPECT_NO_THROW(pass.writePattern(size - sizeof(dummyValue)));
-    EXPECT_THROW(pass.verifyPattern(size), InternalFailure);
+    stdplus::fd::Fd&& writeFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::WriteOnly);
+
+    EXPECT_NO_THROW(pass.writePattern(size - sizeof(dummyValue), writeFd));
+
+    stdplus::fd::Fd&& readFd =
+        stdplus::fd::open(testFileName, stdplus::fd::OpenAccess::ReadOnly);
+
+    EXPECT_THROW(pass.verifyPattern(size, readFd), InternalFailure);
+}
+
+TEST(pattern, shortReadWritePass)
+{
+    std::string testFileName = "testfile_shortRead";
+
+    uint64_t size = 4096;
+    size_t shortSize = 128;
+    Pattern pass(testFileName);
+    auto shortData_1 = std::vector<std::byte>(shortSize);
+    auto shortData_2 = std::vector<std::byte>(shortSize);
+    auto restOfData = std::vector<std::byte>(size - shortSize * 2);
+    stdplus::fd::FdMock mock;
+
+    // test write pattern with short blocks
+    EXPECT_CALL(mock, write(std::span<std::byte> x))
+        .WillOnce([this](std::span<std::byte> x){
+        std::copy_n(x.begin(), shortSize, shortData_1.begin());
+        Return(shortData_1);
+        };
+        .WillOnce([this](std::span<std::byte> x){
+        std::copy_1(x.begin(), restOfData.size(), restOfData.begin());
+        Return(restOfData);
+        });
+        .WillOnce([this](std::span<std::byte> x){
+        std::copy_n(x.begin(), shortSize, shortData_2.begin());
+        Return(shortData_2);
+        });
+
+    EXPECT_NO_THROW(pass.writePattern(size, mock));
+
+    // test read pattern with short blocks
+    EXPECT_CALL(mock, read(_))
+        .WillOnce(Return(shortData_1))
+        .WillOnce(Return(restOfData))
+        .WillOnce(Return(shortData_2));
+
+    EXPECT_NO_THROW(pass.verifyPattern(size, mock));
+}
+/*
+TEST(pattern, shortReadFail)
+{
+    std::string testFileName = "testfile_shortRead";
+
+    uint64_t size = 4096;
+    size_t shortSize = 128;
+    Pattern tryPattern(testFileName);
+    auto shortData = std::vector<std::byte>(shortSize);
+    auto restOfData_1 = std::vector<std::byte>(size - shortSize);
+    auto restOfData_2 = std::vector<std::byte>(size - shortSize);
+    // open the file and write none zero to it
+
+    stdplus::fd::FdMock mock;
+
+    // test writes
+    EXPECT_CALL(mock, write(_))
+        .WillOnce(Return(shortData))
+        .WillOnce(Return(restOfData_1))
+        .WillOnce(Return(restOfData_2)); // return too much data!
+
+    EXPECT_THROW(tryPattern.writePattern(size, mock), InternalFailure);
+
+    // test reads
+    EXPECT_CALL(mock, read(_))
+        .WillOnce(Return(shortData))
+        .WillOnce(Return(restOfData_1))
+        .WillOnce(Return(restOfData_2)); // return too much data!
+
+    EXPECT_THROW(tryPattern.verifyPattern(size, mock), InternalFailure);
+}
+*/
+ACTION(ThrowException)
+{
+    throw InternalFailure();
+}
+
+TEST(pattern, driveIsSmaller)
+{
+    std::string testFileName = "testfile_driveIsSmaller";
+
+    uint64_t size = 4096;
+    Pattern tryPattern(testFileName);
+
+    stdplus::fd::FdMock mocks;
+    testing::InSequence s;
+
+    // test writes
+    EXPECT_CALL(mocks, write(_))
+        .Times(100)
+        .WillRepeatedly(Return(std::vector<std::byte>{}))
+        .RetiresOnSaturation();
+    EXPECT_CALL(mocks, write(_)).WillOnce(ThrowException());
+
+    EXPECT_THROW(tryPattern.writePattern(size, mocks), InternalFailure);
+
+    // test reads
+    EXPECT_CALL(mocks, read(_))
+        .Times(100)
+        .WillRepeatedly(Return(std::vector<std::byte>{}))
+        .RetiresOnSaturation();
+    EXPECT_CALL(mocks, read(_)).WillOnce(ThrowException());
+
+    EXPECT_THROW(tryPattern.verifyPattern(size, mocks), InternalFailure);
 }
 
 } // namespace estoraged_test

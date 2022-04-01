56c5b2d46d74cd9570c241a6a1ed3654057d49e8
John Edward Broadbent <jebr@google.com>
2022-03-31 19:33:21 -0700

    Fix Short read/write issue
    It was possible for "short" reads and writes to cause the pattern and
    zero steps to not work correctly. This change adds logic to deal with
    the short reads.
    
    Change-Id: If8df9bdba159a3bcfa77104a4c17b8d352794db2
    Signed-off-by: John Edward Broadbent <jebr@google.com>

--- include/pattern.hpp
+++ include/pattern.hpp
@@ -11,7 +11,7 @@
 
 namespace estoraged
 {
-using stdplus::fd::ManagedFd;
+using stdplus::fd::Fd;
 
 class Pattern : public Erase
 {
@@ -27,25 +27,38 @@ class Pattern : public Erase
      * and throws errors accordingly.
      *
      *  @param[in] bytes - Size of the block device
+     *  @param[in] fd - the stdplus file descriptor
      */
     void writePattern()
     {
-        writePattern(util::Util::findSizeOfBlockDevice(devPath));
+        stdplus::fd::Fd&& fd =
+            stdplus::fd::open(devPath, stdplus::fd::OpenAccess::WriteOnly);
+        writePattern(util::Util::findSizeOfBlockDevice(devPath), fd);
     }
 
-    void writePattern(uint64_t driveSize);
+    void writePattern(uint64_t driveSize, Fd& fd);
 
     /** @brief verifies the uncompressible random pattern is on the drive
      * and throws errors accordingly.
      *
      *  @param[in] bytes - Size of the block device
+     *  @param[in] fd - the stdplus file descriptor
      */
 
     void verifyPattern()
     {
-        verifyPattern(util::Util::findSizeOfBlockDevice(devPath));
+        stdplus::fd::Fd&& fd =
+            stdplus::fd::open(devPath, stdplus::fd::OpenAccess::ReadOnly);
+        verifyPattern(util::Util::findSizeOfBlockDevice(devPath), fd);
     }
-    void verifyPattern(uint64_t driveSize);
+    void verifyPattern(uint64_t driveSize, Fd& fd);
+
+  private:
+    static constexpr uint32_t seed = 0x6a656272;
+    static constexpr size_t blockSize = 4096;
+    static constexpr size_t blockSizeUsing32 = blockSize / sizeof(uint32_t);
+    static constexpr uint16_t maxRetry = 32;
+    static constexpr uint16_t delay = 1000;
 };
 
 } // namespace estoraged

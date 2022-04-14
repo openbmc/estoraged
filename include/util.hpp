b0d513e9002a5e6d2ac79ccbd05494480384e77b
John Wedig <johnwedig@google.com>
2022-04-13 18:12:25 -0700

    Initial integration with Entity Manager
    This commit changes eStoraged so that it doesn't take a specific device
    as an argument. Instead, it looks for a config object from Entity
    Manager and creates a D-Bus object corresponding to the config object.
    
    The config objects need to expose the following interface:
      "xyz.openbmc_project.Configuration.EmmcDevice"
    
    To support more types of storage devices in the future, we can introduce
    a new interface for each one.
    
    In addition, eStoraged currently only supports 1 eMMC device. If we want
    to support more than one in the future, we will need to add more
    information to the Entity Manager config, to distinguish between them.
    
    Assuming the eMMC is located on a FRU-detectable board, an "Exposes"
    entry can be added to that board's Entity Manager config, for example:
    {
        "Name": "example_emmc",
        "Type": "EmmcDevice"
    }
    
    Doing so will tell Entity Manager to create a config object with the
    EmmcDevice interface mentioned above. Then, eStoraged will find the
    config object with that interface and create its own D-Bus object that
    can be used to manage the eMMC.
    
    Tested:
    Updated the Entity Manager config (as described above), started
    eStoraged, then tested most of its methods and properties using busctl.
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume FormatLuks ays 3 1 2 3 \
      xyz.openbmc_project.Inventory.Item.Volume.FilesystemType.ext4 \
      --timeout=60
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Lock
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Unlock ay 3 1 2 3
    $ busctl get-property xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Locked
    $ busctl get-property xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Drive Capacity
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Erase s \
      xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.VerifyGeometry
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Erase s \
      xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.LogicalOverWrite \
      --timeout=1200
    $ busctl call xyz.openbmc_project.eStoraged \
      /xyz/openbmc_project/inventory/storage/mmcblk0 \
      xyz.openbmc_project.Inventory.Item.Volume Erase s \
      xyz.openbmc_project.Inventory.Item.Volume.EraseMethod.LogicalVerify \
      --timeout=1200
    
    Signed-off-by: John Wedig <johnwedig@google.com>
    Change-Id: If137d02e185c366f4a1437076512b4883ba6d595

--- include/util.hpp
+++ include/util.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include "getConfig.hpp"
+
+#include <filesystem>
 #include <string>
 
 namespace estoraged
@@ -17,6 +20,27 @@ uint64_t findSizeOfBlockDevice(const std::string& devPath);
  *  @return the life remaing for the emmc, as a percentage.
  */
 uint8_t findPredictedMediaLifeLeftPercent(const std::string& sysfsPath);
+
+/** @brief Look for the device described by the provided StorageData.
+ *  @details Currently, this function assumes that there's only one eMMC.
+ *    When we need to support multiple eMMCs, we will put more information in
+ *    in the EntityManager config, to differentiate between them. Also, if we
+ *    want to support other types of storage devices, this function will need
+ *    to be updated.
+ *
+ *  @param[in] data - map of properties from the config object.
+ *  @param[in] searchDir - directory to search for devices in sysfs, e.g.
+ *    /sys/block
+ *  @param[out] deviceFile - device file that was found, e.g. /dev/mmcblk0.
+ *  @param[out] sysfsDir - directory containing the sysfs entries for this
+ *    device.
+ *  @param[out] luksName - name of the encrypted LUKS device.
+ *
+ *  @return True if the device was found. False otherwise.
+ */
+bool findDevice(const StorageData& data, const std::filesystem::path& searchDir,
+                std::filesystem::path& deviceFile,
+                std::filesystem::path& sysfsDir, std::string& luksName);
 
 } // namespace util
 
